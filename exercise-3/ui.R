8247b26c73094e1b3e6f2ff09b05b236a8edb14d
MyungJin Eun <eunmyun@uw.edu>
2017-05-17 16:58:00 -0700

    lab

--- exercise-3/ui.R
+++ exercise-3/ui.R
@@ -2,6 +2,9 @@
 shinyUI(fluidPage(
   mainPanel(
     # Add a selectInput (with a proper id) that allows you to select a variable to map
+    selectInput("select", label = h3("Select box"), 
+                choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
+                selected = 1),
     
     # Use plotlyOutput to show your map
     plotlyOutput('map')
